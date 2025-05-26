b06650c6611268c8a2270aa82ce6ddd3dd6ed305
Krishna <kgsing15@gmail.com>
2025-05-26 22:52:14 +0530

    second commit

---
+++ config/mobile/android.info.ts
@@ -0,0 +1,11 @@
+class AndroidInfo {
+    static deviceName() {
+        return "Pixel 6";
+    }
+    static androidVersion() {
+        return "14.0";
+    }
+
+}
+
+export const androidInfo = new AndroidInfo();
